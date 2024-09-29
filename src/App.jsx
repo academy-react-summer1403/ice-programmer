231bf850f88877cb617a6947ac5a67b63a91b10a
Oveiss <oveissershadian@gmail.com>
2024-09-29 09:59:43 +0330

    create project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+
+
+
+const App = ()=>{
+  return (
+    <div className='App'>
+    </div>
+  )
+  
+}
+
+export default App;
+
+
+
+
+
+
