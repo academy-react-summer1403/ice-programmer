83db09b98d59b44cac344ca28088b6b5c808647b
Oveiss <oveissershadian@gmail.com>
2024-09-30 11:05:04 +0330

    compelete forms

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,5 +1,6 @@
 // import { Fragment } from "react"
 import { useState } from "react"
+import {Formik, Form} from 'formik';
 
 
 const SignUp =()=>{
@@ -16,26 +17,52 @@ const SignUp =()=>{
     const [Date, setDate] = useState("")
     console.log(Date)
 
-
+    const onSubmit =(event)=>{
+        event.preventDefault();
+        console.log('Form submited', Password, Name, Date, PhoneNumber)
+    }
 
 
     return(
         <div className="mt-5 ml-5">
-         <form>
-         <input type="text" name="PhoneNumber" placeholder="...شماره همراه"onChange={(e)=>setPhoneNumber(e.target.value)} className=" d-rtl border border-solid border-gray p-3 rounded-xl w-60 shadow-md hover:shadow-purple-500 "></input>
-         </form>
+            <Formik>
+                <Form>
+                    <input 
+                    type="text" 
+                    name="PhoneNumber" 
+                    placeholder="...شماره همراه"
+                    value={PhoneNumber}
+                    onChange={(e)=>setPhoneNumber(e.target.value)}
+                    className=" ml-4 d-rtl border border-solid border-gray p-3 rounded-xl w-60 shadow-md hover:shadow-purple-500 "></input>
+                    
+                
+                    <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="رمز ورود " 
+                    value={Password}
+                    onChange={(e)=>setPassword(e.target.value)} 
+                    className="ml-4 border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
+                    
+                    <input 
+                    type="text" 
+                    name="name" 
+                    placeholder=" نام" 
+                    value={Name}
+                    onChange={(e)=>setName(e.target.value)}
+                    className="ml-4 border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
+                    
+                    <input 
+                    type="date" 
+                    name="date" 
+                    value={Date}
+                    onChange={(e)=>setDate(e.target.value)}
+                    className="ml-4 border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
 
-         <form className='mt-6'>
-         <input type="password" name="password" placeholder="رمز ورود " onChange={(e)=>setPassword(e.target.value)} className="border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
-         </form>
-
-         <form className='mt-6'>
-         <input type="text" name="name" placeholder=" نام" onChange={(e)=>setName(e.target.value)} className="border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
-         </form>
-
-         <form className='mt-6'>
-         <input type="date" name="date" onChange={(e)=>setDate(e.target.value)} className="border border-solid border-gray p-3 rounded-xl w-60 shadow-md "></input>
-         </form>
+                    <button type='Submit'>Submit</button>
+                </Form>
+            </Formik>
+        
         </div>
     )
 }
