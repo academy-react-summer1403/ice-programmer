b119ec6c467b2d11e1bb98d7f882b2f61486ab55
Oveiss <oveissershadian@gmail.com>
2024-10-01 14:56:37 +0330

    add logo

--- src/Components/Common/Header/Header.jsx
+++ src/Components/Common/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <div className='border h-[120px] flex'>
-        <div className='border w-1/4'> logo</div>
+        <div className='border w-1/4'>
+            <img src='../public/Pics/Logo.jpg' className='w-26 h-[100%] mr-[10%]'></img>
+        </div>
         <div className='border w-2/4 flex items-end justify-center gap-[15px]'>
         <a href='#' className='mb-3'>صفحه نخست</a>
         <a href='#' className='mb-3'>دوره ها</a>
