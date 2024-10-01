718905c52115a6ff4f00f27a07e30c7d834902dd
Oveiss <oveissershadian@gmail.com>
2024-10-01 17:53:34 +0330

    header

--- src/Components/Common/Header/Header.jsx
+++ src/Components/Common/Header/Header.jsx
@@ -3,17 +3,23 @@ import React from 'react'
 function Header() {
   return (
     <div className='border h-[120px] flex'>
-        <div className='border w-1/4'>
-            <img src='../public/Pics/Logo.jpg' className='w-26 h-[100%] mr-[10%]'></img>
+        <div className='w-1/4'>
+            <img src='../public/Pics/Logo.jpg' className='w-24 h-[84%] mr-[15%] mt-[10px] rounded-3xl'></img>
         </div>
-        <div className='border w-2/4 flex items-end justify-center gap-[15px]'>
-        <a href='#' className='mb-3'>صفحه نخست</a>
-        <a href='#' className='mb-3'>دوره ها</a>
-        <a href='#'className='mb-3'>اساتید</a>
-        <a href='#'className='mb-3'>اخبار و مقالات</a>
-        <a href='#'className='mb-3'>تماس با ما</a>
+        <div className='w-2/4 flex items-end justify-center gap-[15px]'>
+            <a href='#' className='mb-3 hover:border-b-2 border-orange-500'>صفحه نخست</a>
+            <a href='#' className='mb-3 hover:border-b-2 border-orange-500'>دوره ها</a>
+            <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>اساتید</a>
+            <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>اخبار و مقالات</a>
+            <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>تماس با ما</a>
         </div>
-        <div className='border w-1/4'></div>
+        <div className='w-1/4 flex justify-center items-end gap-5'>
+            <img src='../public/Pics/Like.empty.png' className='w-6 h-6 mb-[10px]'></img>
+            <a href='#' className='w-6 h-6 mb-[10px]'><img src='../public/Pics/bag.png'></img></a>
+            <button className='bg-[#ff8A00] text-[11px] pl-3 pt-[5px] rounded-2xl w-28 h-8 mb-[10px]'>حساب کاربری
+                <img src='../public/Pics/person.png' className='w-6 h-6 relative right-[85px] bottom-5'></img>
+            </button>
+        </div>
     </div>
   )
 }
