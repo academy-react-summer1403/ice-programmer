ce522d2b6170413bc484cfd59df563dc8a2cd8dd
Oveiss <oveissershadian@gmail.com>
2024-10-01 18:37:33 +0330

    header

--- src/Components/Common/Header/Header.jsx
+++ src/Components/Common/Header/Header.jsx
@@ -13,9 +13,10 @@ function Header() {
             <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>اساتید</a>
             <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>اخبار و مقالات</a>
             <a href='#'className='mb-3 hover:border-b-2 border-orange-500'>تماس با ما</a>
+      
         </div>
         <div className='w-1/4 flex justify-center items-end gap-5'>
-            <img src='../public/Pics/Like.empty.png' className='w-6 h-6 mb-[10px]'></img>
+            <img src='../public/Pics/Like.empty.png' className='w-6 h-6 mb-[10px] cursor-pointer'></img>
             <a href='#' className='w-6 h-6 mb-[10px]'><img src='../public/Pics/bag.png'></img></a>
             <button className='bg-[#ff8A00] text-[11px] pl-3 pt-[5px] rounded-2xl w-28 h-8 mb-[10px]'>حساب کاربری
                 <img src='../public/Pics/person.png' className='w-6 h-6 relative right-[85px] bottom-5'></img>
@@ -26,3 +27,6 @@ function Header() {
 }
 
 export default Header
+
+
+   
