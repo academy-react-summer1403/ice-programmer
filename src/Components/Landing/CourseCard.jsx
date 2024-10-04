489123fdc3c42fa7dae40a4ee50a4ce97aadf019
oveiss <oveiss.ershadian@gmail.com>
2024-10-04 15:26:23 +0330

    making component

--- src/Components/Landing/CourseCard.jsx
+++ src/Components/Landing/CourseCard.jsx
@@ -1,1 +1,16 @@
-console
+// import React from 'react'
+
+function CourseCard() {
+  return (
+    <div className="w-[278px] h-[360px] bg-white rounded-[20px]">
+        <img src="public/Pics/01.jpg" className="w-[92%] mx-auto relative bottom-6 rounded-3xl"></img>
+        <h1 className="text-[14px] font-bold mr-1 relative bottom-3">آموزش  پیشرفته ری اکت</h1>
+        <p className="w-[97%] h-20 mr-1 text-[13px] overflow-hidden"> ک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش تلابس لابلسل نتنتس یک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش  تلابس لابلسل نتنتس نتانتسیابس تنانست  </p>
+        <ul className=" mr-1 relative top-8 text-[13px] list-disc list-inside text-[#0004ff]"><li>استاد شماره یک</li></ul>
+        <span className="relative top-16 mr-1 bg-slate-400 rounded-2xl text-[15px] p-1 pl-5 bg-[url(public/Pics/timer.png)] bg-no-repeat bg-left [background-size:20px]">10:20:40</span>
+        <span className="relative top-16 right-[120px] text-red-700">!رایگان</span>
+    </div>
+  )
+}
+
+export default CourseCard;
--- src/Components/Landing/CoursePage.jsx
+++ src/Components/Landing/CoursePage.jsx
@@ -1,4 +1,6 @@
 import {Formik, Field} from 'formik';
+import CourseCard from './CourseCard';
+import { Link } from 'react-router-dom';
 
 function CoursePage() {
   return (
@@ -13,44 +15,16 @@ function CoursePage() {
                 <Field className='p-5 mr-[1%] bg-[url(src/assets/search.svg)] bg-no-repeat bg-left text-base font-bold [background-size:35px] shadow-inner shadow-slate-400 rounded-xl' type='text' placeholder='جستجوی دوره' name='SerachCourse'  ></Field>
                 
                 <h1 className="text-[18px] font-bold">مرتب سازی</h1>
-                <a href="#">جدیدترین</a>
-                <a href="#">پرفروش ترین</a>
-                <a href="#">گران ترین</a>
-                <a href="#">ارزان ترین</a>
+                <a href="">جدیدترین</a>
+                <a href="">پرفروش ترین</a>
+                <a href="">گران ترین</a>
+                <a href="">ارزان ترین</a>
             </div>
             <div className='w-[74%] h-auto mt-6 pt-5 flex flex-row flex-wrap justify-between gap-y-24'>
-                <div className="w-[278px] h-[360px] bg-white rounded-[20px]">
-                    <img src="public/Pics/01.jpg" className="w-[92%] mx-auto relative bottom-6 rounded-3xl"></img>
-                    <h1 className="text-[14px] font-bold mr-1 relative bottom-3">آموزش  پیشرفته ری اکت</h1>
-                    <p className="w-[97%] h-20 mr-1 text-[13px] overflow-hidden"> ک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش تلابس لابلسل نتنتس یک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش  تلابس لابلسل نتنتس نتانتسیابس تنانست  </p>
-                    <ul className=" mr-1 relative top-8 text-[13px] list-disc list-inside text-[#0004ff]"><li>استاد شماره یک</li></ul>
-                    <span className="relative top-16 mr-1 bg-slate-400 rounded-2xl text-[15px] p-1 pl-5 bg-[url(public/Pics/timer.png)] bg-no-repeat bg-left [background-size:20px]">10:20:40</span>
-                    <span className="relative top-16 right-[120px] text-red-700">!رایگان</span>
-                </div>
-                <div className="w-[278px] h-[360px] bg-white rounded-[20px]">
-                <img src="public/Pics/01.jpg" className="w-[92%] mx-auto relative bottom-6 rounded-3xl"></img>
-                    <h1 className="text-[14px] font-bold mr-1 relative bottom-3">آموزش  پیشرفته ری اکت</h1>
-                    <p className="w-[97%] h-20 mr-1 text-[13px] overflow-hidden"> ک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش تلابس لابلسل نتنتس یک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش  تلابس لابلسل نتنتس نتانتسیابس تنانست  </p>
-                    <ul className=" mr-1 relative top-8 text-[13px] list-disc list-inside text-[#0004ff]"><li>استاد شماره یک</li></ul>
-                    <span className="relative top-16 mr-1 bg-slate-400 rounded-2xl text-[15px] p-1 pl-5 bg-[url(public/Pics/timer.png)] bg-no-repeat bg-left [background-size:20px]">10:20:40</span>
-                    <span className="relative top-16 right-[120px] text-red-700">!رایگان</span>
-                </div>
-                <div className="w-[278px] h-[360px] bg-white rounded-[20px]">
-                <img src="public/Pics/01.jpg" className="w-[92%] mx-auto relative bottom-6 rounded-3xl"></img>
-                    <h1 className="text-[14px] font-bold mr-1 relative bottom-3">آموزش  پیشرفته ری اکت</h1>
-                    <p className="w-[97%] h-20 mr-1 text-[13px] overflow-hidden"> ک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش تلابس لابلسل نتنتس یک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش  تلابس لابلسل نتنتس نتانتسیابس تنانست  </p>
-                    <ul className=" mr-1 relative top-8 text-[13px] list-disc list-inside text-[#0004ff]"><li>استاد شماره یک</li></ul>
-                    <span className="relative top-16 mr-1 bg-slate-400 rounded-2xl text-[15px] p-1 pl-5 bg-[url(public/Pics/timer.png)] bg-no-repeat bg-left [background-size:20px]">10:20:40</span>
-                    <span className="relative top-16 right-[120px] text-red-700">!رایگان</span>
-                </div>
-                <div className="w-[278px] h-[360px] bg-white rounded-[20px]">
-                <img src="public/Pics/01.jpg" className="w-[92%] mx-auto relative bottom-6 rounded-3xl"></img>
-                    <h1 className="text-[14px] font-bold mr-1 relative bottom-3">آموزش  پیشرفته ری اکت</h1>
-                    <p className="w-[97%] h-20 mr-1 text-[13px] overflow-hidden"> ک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش تلابس لابلسل نتنتس یک متن برای ازمایش یک متن برای آزمایش یک متن برای ازمایش  تلابس لابلسل نتنتس نتانتسیابس تنانست  </p>
-                    <ul className=" mr-1 relative top-8 text-[13px] list-disc list-inside text-[#0004ff]"><li>استاد شماره یک</li></ul>
-                    <span className="relative top-16 mr-1 bg-slate-400 rounded-2xl text-[15px] p-1 pl-5 bg-[url(public/Pics/timer.png)] bg-no-repeat bg-left [background-size:20px]">10:20:40</span>
-                    <span className="relative top-16 right-[120px] text-red-700">!رایگان</span>
-                </div>
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />                
             </div>
             <div className='w-[310px] h-96 mt-[19px] ml-5 rounded-[20px] bg-white'>
                 <ul className='w-[93%] m-[5px_auto]' >فیلترها
