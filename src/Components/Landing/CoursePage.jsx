66d0f9c84f4bd0f9fd44244e51079a7bdd796fa3
oveiss <oveiss.ershadian@gmail.com>
2024-10-04 17:56:39 +0330

    !important

--- src/Components/Landing/CoursePage.jsx
+++ src/Components/Landing/CoursePage.jsx
@@ -1,4 +1,4 @@
-import {Formik, Field} from 'formik';
+import {Formik, Field, Form} from 'formik';
 import CourseCard from './CourseCard';
 import LinkComponent from './LinkComponent';
 
@@ -6,7 +6,8 @@ function CoursePage() {
   return (
     <div className='bg-[#a09f9f] '>
         <Formik>
-        <div className='flex flex-row-reverse flex-wrap gap-x-2 gap-y-4 w-[90%] mx-[auto]'>
+            <Form>
+            <div className='flex flex-row-reverse flex-wrap gap-x-2 gap-y-4 w-[90%] mx-[auto]'>
             <div className='border-[1px_solid_red] w-[100%] flex justify-between'>
                 <h1>دوره ها</h1>
                 <h1> 5 دوره آموزشی</h1>
@@ -14,7 +15,7 @@ function CoursePage() {
             <div className='w-[100%] h-[115px] bg-white rounded-[20px] flex items-center mt-[10px] gap-5'>
                 <Field className='p-5 mr-[1%] bg-[url(src/assets/search.svg)] bg-no-repeat bg-left text-base font-bold [background-size:35px] shadow-inner shadow-slate-400 rounded-xl' type='text' placeholder='جستجوی دوره' name='SerachCourse'  ></Field>
                 
-                <div className="text-[18px] font-bold">مرتب سازی</div>
+                <h1 className="text-[18px] font-bold">مرتب سازی</h1>
                 <LinkComponent id={'LastProduct'} topic={'جدیدترین'} />
                 <LinkComponent id={'BestSelling'} topic={'پرفروش ترین'} />
                 <LinkComponent id={'Expensive'} topic={'گران ترین'} />
@@ -35,6 +36,7 @@ function CoursePage() {
                 </ul>
             </div>
         </div>
+            </Form>
         </Formik>
     </div>
   )
