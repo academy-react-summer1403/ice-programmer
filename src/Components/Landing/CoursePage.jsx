9727dcd15952068aefb6c6cd018957ebddae5c05
oveiss <oveiss.ershadian@gmail.com>
2024-10-06 22:29:18 +0330

    add accardion

--- src/Components/Landing/CoursePage.jsx
+++ src/Components/Landing/CoursePage.jsx
@@ -64,87 +64,41 @@ const CoursePage=()=> {
                     );   
                 })}             
             </div>
-
             <div className='w-[310px] h-96 mt-[19px] ml-5 rounded-[20px] bg-white'> 
-            {/* <Formik>
-                <Form>
-                <ul id="accardion" className="duration-500 p-[5px] pb-[20px] list-none text-[17px]">
-                    <li className=' relative rounded-[5px] m-[15px_0]'>
-                        <input type="checkbox" id="c0" name="na"  checked/>
-                        <label htmlFor="c0" className='inline-block'>  <span className="tip"> قیمت </span></label>
-                        <div className="content">
-
-                            <output id="priFromOut">0</output>
-                            <span> از : </span>
-                            <input type="range" id="priFrom" min="0" max="1000000" step="10000" value="0"/>
-                            <output id="priToOut">1000000</output>
-                            <span> تا : </span>
-                            <input type="range" id="priTo" min="0" max="1000000" step="10000" value="1000000"/>
-                        
-                        </div>
-
-                    </li>
-                    <li className='relative rounded-[5px] m-[15px_0]'>
-                        <input type="checkbox" id="c1" name="na"/>
-                        <label htmlFor="c1" className='inline-block'><span className="tip">  دسته بندی </span></label>
-                        <div className="content">
-                            
-                            <input type="radio" id="cat1" name="ca"/>
-                            <label htmlFor="cat1"> آرایشی </label>
-                            
-                            <input type="radio" id="cat2" name="ca"/>
-                            <label htmlFor="cat2"> بهداشتی </label>	
-
-                            <input type="radio" id="cat3" name="ca" checked/>
-                            <label htmlFor="cat3"> همه </label>				
-                        </div>
-
-                    </li> 
-                    <li className='relative rounded-[5px] m-[15px_0]'>
-                        <input type="checkbox" id="ch5" name="na" />
-                        <label htmlFor="ch5" className='inline-block'><span className="tip"> موجودی </span> </label>
-                        <div className="content">
-                            <input type="radio" id="exi1" name="ex"/>
-                            <label htmlFor="exi1">  موجود  </label>	
-
-                            <input type="radio" id="exi2" name="ex" checked/>
-                            <label htmlFor="exi2"> همه </label>	
-                        </div>
-
-                    </li>
-                </ul>
-                </Form>
-                </Formik> */}
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="checkbox" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="checkbox" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="checkbox" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>               
             </div>
         </div>
-
+        <div className='w-[100%] text-center'>
+            <div className="join my-5">
+                <button className="join-item btn">1</button>
+                <button className="join-item btn btn-active">2</button>
+                <button className="join-item btn">3</button>
+                <button className="join-item btn">4</button>
+            </div>
+        </div>
+        
     </div>
   )
 } 
 
 export default CoursePage;
-
-
-
-
-
-/* <ul className='w-[93%] m-[5px_auto]' >فیلترها
-<li className=" mt-3 border border-[#ddd] p-5 rounded-[13px]">
-    
-    <Field type="checkbox" id="c1" name="na" className='hidden' checked />
-    <label htmlFor="c1"  className='cursor-pointer block'><span className="tip">استاد  </span></label>
-    <div className="h-[auto]">                          
-        <Field type="radio" id="cat1" name="ca" className='hidden' />
-        <label htmlFor="cat1" className='block'> فیلتر یک </label>
-        
-        <Field type="radio" id="cat2" name="ca" className='hidden'/>
-        <label htmlFor="cat2" className='block'> فیلتر دو </label>	
-    
-        <Field type="radio" id="cat3" name="ca" checked className='hidden'/>
-        <label htmlFor="cat3" className='block'> فیلتر سه </label>			
-    </div>
-</li>
-<li className="mt-3 border border-[#ddd] p-5 rounded-[13px]"></li>
-<li className="mt-3 border border-[#ddd] p-5 rounded-[13px]"></li>
-<li className=" mt-3 border border-[#ddd] p-5 rounded-[13px]"></li>
-</ul> */
